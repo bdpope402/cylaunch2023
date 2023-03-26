3f2e96637655f76d9cd2c26ed9aad5f38161d3f6
cylaunchPi <cylaunch@iastate.edu>
2023-03-26 20:03:11 +0100

    Added logger and flag to launch_sm

--- launch_sm.py
+++ launch_sm.py
@@ -2,26 +2,35 @@ import rocketcheck
 import execute_instruction
 import time
 import os
+from cyllogger import cyllogger 
 
 WAIT_TIME = 1800 #30 minute sleep time
 SLEEP_FILE_PATH = "/home/cylaunch/payload_code/flags/dosleep.cyl"
+SM_FILE_PATH= "/home/cylaunch/payload_code/flags/doLaunchOnStartup.cyl"
+
+log = cyllogger("sm_log")
 
 def main():
-    print("In main")
+    log.writeTo("In main")
     if(os.path.exists(SLEEP_FILE_PATH) == False):
-        print("Entering launchpad sleep")
+        log.writeTo("Entering launchpad sleep")
         time.sleep(WAIT_TIME)
     else:
-        print("skipping launchpad sleep")
+        log.writeTo("skipping launchpad sleep")
     while(rocketcheck.checkIfLaunching() == False):
         time.sleep(1)
-    print("Exited launch loop")
+    log.writeTo("Exited launch loop")
     time.sleep(5)
-    print("Starting landed loop")
+    log.writeTo("Starting landed loop")
     while(rocketcheck.checkIfLanded() == False):
         time.sleep(1)
-    print("Exited landing loop")
+    log.writeTo("Exited landing loop")
+
+    #find upright door, launch fm_rtl and multimon
 
 
 if __name__ == "__main__":
-    main()
+    if(os.path.exists(SLEEP_FILE_PATH) == True):
+        main()
+    else:
+        log.writeTo("doLaunchOnStartup.cyl not found, not entering loop")

a9b4b11772eb201e5b3b56cc75ebb9c2f87442ce
cylaunchPi <cylaunch@iastate.edu>
2023-03-28 21:59:44 +0100

    Added move and moveLoop to allow manual control of stepper motors and lin actuator

---
+++ moveLoop.py
@@ -0,0 +1,22 @@
+from move import move
+import time
+
+def main():
+    while(True == True):
+        userInput = input("What motor do you want? (S for stepepr L for lin act)")
+        userDir = input("F or B?")
+        userSteps = int(input("How many steps:"))
+        if(userInput == "S"):
+            if(userDir == "F"):
+                move.spinF(userSteps)
+            elif(userDir == "B"):
+                move.spinB(userSteps)
+        elif(userInput == "L"):
+            if(userDir == "F"):
+                move.extendF(userSteps)
+            elif(userDir == "B"):
+                move.extendB(userSteps)
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()

3f94a1510cc13683873c837e73e1e9bc3e8c46f8
cylaunchPi <cylaunch@iastate.edu>
2023-04-01 17:10:38 +0100

    Updated gitignore, tried interupt script. Updated move to doublestep

--- move.py
+++ move.py
@@ -1,36 +1,39 @@
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 
-class move:
 
+class move:
     def spinF(steps):
         kit = MotorKit()
         i = 0
-        while(i < steps):
-            kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
+        while i < steps:
+            kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
             i += 1
         kit.stepper1.release()
 
     def spinB(steps):
         kit = MotorKit()
         i = 0
-        while(i < steps):
-            kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
+        while i < steps:
+            kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
             i += 1
         kit.stepper1.release()
 
     def extendF(steps):
         kit = MotorKit()
         i = 0
-        while(i < steps):
+        while i < steps:
             kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
             i += 1
         kit.stepper2.release()
-    
+
     def extendB(steps):
         kit = MotorKit()
         i = 0
-        while(i < steps):
+        while i < steps:
             kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
-            i+= 1
+            i += 1
         kit.stepper2.release()
+
+
+# move.spinF(200)
